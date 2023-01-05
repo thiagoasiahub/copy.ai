a9a68ffa4a610e739557c3bca2d57ab8c8008426
unknown <thiago@asiahub.hk>
2023-01-05 15:40:06 -0300

    Sgortening the localhost url

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-    const response = await fetch('http://localhost:3000', {
+    const response = await fetch('/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
