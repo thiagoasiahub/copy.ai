f734261054f625f40e95633095831384176d8ad3
unknown <thiago@asiahub.hk>
2023-01-05 18:57:05 -0300

    OpenAi url

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-    const response = await fetch('http://localhost:3000', {
+    const response = await fetch('https://api.openai.com/v1/completions', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
