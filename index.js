298a8a5f105726c9bd915da849b20a51a62f3799
unknown <thiago@asiahub.hk>
2023-01-05 14:57:26 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+//A express server, which will handle api requests coming in and reponde back with a json object, it will use body parser as well as cors
+
+const OpenAI = require('openai');
+const { Configuration, OpenAIApi } = OpenAI;
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+const configuration = new Configuration({
+    organization: "org-5ApOj8L4XTSdXVpK7BJsLBkV",
+    apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/', async (req, res) => {
+    const { message, message1, message2 } = req.body;
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `Escreva um ${message2} criativo para o seguinte produto destinado aos ${message}: Product: ${message1}`,
+        max_tokens: 1000,
+        temperature: 1,
+      });
+        console.log(response.data)
+        if (response.data.choices) {
+            res.json({
+                message: response.data.choices[0].text
+            });
+        }
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ src/App.js
@@ -0,0 +1,61 @@
+// Create a react component that inputs two textarea messages and one drop down menu with two options first Facebook second Taboola then performs a fetch request to localhost:3000 gets back a response as a data.message and data.message1 and displays that message in a box below
+
+import React, { useState } from 'react';
+import './App.css';
+import * as ReactBootstrap from 'react-bootstrap';
+
+function App() {
+  const [message, setMessage] = useState('');
+  const [message1, setMessage1] = useState('');
+  const [message2, setMessage2] = useState('');
+  const [response, setResponse] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const response = await fetch('http://localhost:3000', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ message, message1, message2 }),
+    });
+    const data = await response.json();
+    setResponse(data.message);
+    setLoading(false);
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+      <label>
+          <select
+            value={message2}
+            onChange={(e) => setMessage2(e.target.value)}
+          >
+            <option value="Artigo">Artigo</option>
+            <option value="Blog Post">Blog Post</option>
+            <option value="Facebook">Facebook</option>
+          </select>
+        </label>
+        <label>
+          <textarea
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </label>
+        <label>
+          <textarea
+            value={message1}
+            onChange={(e) => setMessage1(e.target.value)}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      {loading ? <ReactBootstrap.Spinner animation="border" variant="warning" /> : <p>{response}</p>}
+    </div>
+  );
+}
+
+export default App;
