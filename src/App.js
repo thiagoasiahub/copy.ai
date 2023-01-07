f7577d5540672f5f814936af80a6d0e99ad9f5bc
unknown <thiago@asiahub.hk>
2023-01-07 15:55:20 -0300

    Port change

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-    const response = await fetch('https://15.228.100.172/:3000', {
+    const response = await fetch('3000', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
